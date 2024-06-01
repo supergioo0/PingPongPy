aa603a83d94c26b2d40e269f768545717d2b9f78
supergioo0 <158353879+supergioo0@users.noreply.github.com>
2024-06-01 13:27:58 +0300

    Update pong.py
    Replaced pngs with surfaces for more accessibility and increased speed

--- pong.py
+++ pong.py
@@ -2,9 +2,10 @@ from pygame import *
 
 #parent class for sprites
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+    def __init__(self, color, player_x, player_y, player_speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (wight, height)) #e.g. 55,55 - parameters
+        self.image = pygame.Surface((width, height))
+        self. image.fill(color)
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -42,17 +43,17 @@ clock = time.Clock()
 FPS = 60
 
 #creating ball and paddles   
-racket1 = Player('racket.png', 30, 200, 4, 50, 150) 
-racket2 = Player('racket.png', 520, 200, 4, 50, 150)
-ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+racket1 = Player((255, 255, 255), 30, 200, 4, 20, 150) 
+racket2 = Player((255, 255, 255), 520, 200, 4, 20, 150)
+ball = GameSprite((255, 255, 255), 200, 200, 4, 50, 50)
 
 font.init()
 font = font.Font(None, 35)
 lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
-speed_x = 3
-speed_y = 3
+speed_x = 4
+speed_y = 4
 
 while game:
     for e in event.get():
